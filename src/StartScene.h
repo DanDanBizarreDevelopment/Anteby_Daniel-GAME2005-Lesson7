86b8cae0de53a35ff337103618891674a7852f37
Daniel Anteby <Daniel.Anteby@gmail.com>
2019-10-18 18:31:58 -0400

    Add project files.

---
+++ src/StartScene.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef __START_SCENE__
+#define __START_SCENE__
+
+#include "Scene.h"
+#include "Label.h"
+#include "ship.h"
+
+class StartScene : public Scene
+{
+public:
+	StartScene();
+	~StartScene();
+
+	// Inherited via Scene
+	virtual void draw() override;
+	virtual void update() override;
+	virtual void clean() override;
+	virtual void handleEvents() override;
+	virtual void start() override;
+
+private:
+	/*Label* m_pStartLabel;
+	Label* m_pInstructionsLabel;*/
+
+	Ship* m_pShip;
+
+	glm::vec2 m_mousePosition;
+
+	// ImGui utility functions
+	void m_ImGuiKeyMap();
+	void m_ImGuiSetStyle();
+	void m_updateUI();
+
+	// ImGui menu variables
+	bool m_exitApp = false;
+	bool m_displayAbout = false;
+	bool m_displayUI = true;
+};
+
+#endif /* defined (__START_SCENE__) */
